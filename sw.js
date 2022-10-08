c2b0508a371e4aa6730822fb9e39cd6224616472
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2022-10-08 15:03:42 -0500

    56 respuesta offline basica

--- sw.js
+++ sw.js
@@ -1,3 +1,18 @@
+self.addEventListener('fetch', event => {
+
+    const offlineResp = new Response(`
+    
+        Bienvenido a mi Página Web
+        necesitas conexion a internet
+    
+    `);
 
 
 
+    const resp = fetch(event.request)
+        .catch(() => offlineResp);
+
+
+    event.respondWith(resp);
+
+});
