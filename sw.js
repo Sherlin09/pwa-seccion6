ce9120f259a62d716139ff6717407ac6f2d713a0
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2022-10-10 14:57:19 -0500

    62 cache dinamico

--- sw.js
+++ sw.js
@@ -1,11 +1,16 @@
-const CACHE_NAME = 'cache-1';
+//const CACHE_NAME = 'cache-1';
+const CACHE_STATIC_NAME = 'static-v1';
+const CACHE_DYNAMIC_NAME = 'dynamic-v1';
+const CACHE_INMUTABLE_NAME = 'inmutable-v1';
+
+const CACHE_DYNAMIC_LIMIT = 50;
 
 
 
 self.addEventListener('install', e => {
 
 
-    const cacheProm = caches.open(CACHE_NAME)
+    const cacheProm = caches.open(CACHE_STATIC_NAME)
         .then(cache => {
 
             return cache.addAll([
@@ -14,14 +19,16 @@ self.addEventListener('install', e => {
                 '/css/style.css',
                 '/img/main.jpg',
                 '/js/app.js',
-                'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'
             ]);
 
 
         });
 
+    const cacheInmutable = caches.open(CACHE_INMUTABLE_NAME)
+        .then(cache => cache.add('https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'));
 
-    e.waitUntil(cacheProm);
+
+    e.waitUntil(Promise.all([cacheProm, cacheInmutable]));
 
 });
 
@@ -37,10 +44,11 @@ self.addEventListener('fetch', e => {
             if (res) return res;
 
             // no esta el archivo
+            console.log('No existe', e.request.url);
 
             return fetch(e.request).then(newResp => {
 
-                caches.open(CACHE_NAME)
+                caches.open(CACHE_DYNAMIC_NAME)
                     .then(cache => {
                         cache.put(e.request, newResp);
                     });
